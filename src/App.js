567e26e3365e70901384b6dc827ad89ded524c75
sougata <sougata19582001@gmail.com>
2024-07-25 01:22:28 +0530

    connected to mysql

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Navbar from "./Navbar";
 
 function App() {
+  const [menus, setMenus] = useState(null);
+
+  useEffect(function () {
+    async function fetchMenus() {
+      const res = await fetch("http://localhost:8000/api/data");
+      const data = await res.json();
+      setMenus(data);
+    }
+    fetchMenus();
+  }, []);
+
+  if (menus === null) return <div>Loading...</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar menus={menus} />
+      <div className="container">React App</div>
+    </>
   );
 }
 
---
+++ src/Navbar.js
@@ -0,0 +1,40 @@
+function Navbar({ menus }) {
+  const { data } = menus;
+  return (
+    <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+      <div className="container-fluid container">
+        <div className="navbar-brand">Dashboard</div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            {data.map((menu) => (
+              <li className="nav-item" key={menu.menuid}>
+                <a
+                  className={`nav-link ${
+                    menu.enable_yn === "N" ? "disabled" : ""
+                  }`}
+                  aria-current="page"
+                  href={`/${menu.menu_name}`}
+                >
+                  {menu.menu_heading}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
